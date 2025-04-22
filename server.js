89af6002e3d18e2e557e4a2ab639ba413da892dc
Luis Rojas <lrojashernandez@byupathway.edu>
2025-04-22 07:09:51 -0500

    url fixed

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const lesson1Controller = require('./controllers/lesson1')
 app.get('/', (req,res) =>{
     res.send('HOLA ESTOY RECORDANDO NODE.JS USANDO EL PUERTO '+ port);
 });
-app.get('./luis',lesson1Controller.luis);
-app.get('./sandra',lesson1Controller.sandra);
+app.get('/luis',lesson1Controller.luis);
+app.get('/sandra',lesson1Controller.sandra);
 
 
 
