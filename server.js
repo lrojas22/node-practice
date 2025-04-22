d4b8aa32df81f7a0afa6dc0baa839b2130267fc5
Luis Rojas <lrojashernandez@byupathway.edu>
2025-04-22 07:03:06 -0500

    sandra first variable

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get('/', (req,res) =>{
     res.send('HOLA ESTOY RECORDANDO NODE.JS USANDO EL PUERTO '+ port);
 });
 app.get('./luis',lesson1Controller.luis);
+app.get('./sandra',lesson1Controller.sandra);
 
 
 
