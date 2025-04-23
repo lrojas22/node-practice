4a5a07bac3f96136641cc20546e97f7c7db31128
Luis Rojas <lrojashernandez@byupathway.edu>
2025-04-23 18:01:03 -0500

    rest

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const routes = require('express').Router();
+/*const routes = require('express').Router();
 
 const lesson1Controller = require('../controllers/lesson1')
 
@@ -8,4 +8,18 @@ app.get('/', (req,res) =>{
 app.get('/luis',lesson1Controller.luis);
 app.get('/sandra',lesson1Controller.sandra);
 
-module.exports = routes;
+module.exports = routes;*/
+
+// routes/index.js
+const express = require('express');
+const router = express.Router();
+
+const lesson1Controller = require('../controllers/lesson1');
+
+router.get('/', (req, res) => {
+  res.send('HOLA ESTOY RECORDANDO NODE.JS USANDO EL PUERTO 3000');
+});
+router.get('/luis', lesson1Controller.luis);
+router.get('/sandra', lesson1Controller.sandra);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //express web server
 const express = require('express');
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
+const routes = require('./routes'); 
+app.use('/', routes);
 
-app.use('/', require('./routes'));
 
-
-app.listen(process.env.PORT || port);
-console.log('HOLA ESTOY RECORDANDO NODE.JS USANDO EL PUERTO ' + (process.env.PORT || port))
+app.listen(port);
+console.log('HOLA ESTOY RECORDANDO NODE.JS USANDO EL PUERTO ' + port)
