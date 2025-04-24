996edea1d878d335cbdff11537c3f5f971bf6076
Luis Rojas <lrojashernandez@byupathway.edu>
2025-04-23 19:40:40 -0500

    conect mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,6 @@
-/*const routes = require('express').Router();
-
-const lesson1Controller = require('../controllers/lesson1')
-
-app.get('/', (req,res) =>{
-    res.send('HOLA ESTOY RECORDANDO NODE.JS USANDO EL PUERTO '+ port);
-});
-app.get('/luis',lesson1Controller.luis);
-app.get('/sandra',lesson1Controller.sandra);
-
-module.exports = routes;*/
-
-// routes/index.js
 const express = require('express');
 const router = express.Router();
 
-const lesson1Controller = require('../controllers/lesson1');
-
-router.get('/', (req, res) => {
-  res.send('HOLA ESTOY RECORDANDO NODE.JS USANDO EL PUERTO 3000');
-});
-router.get('/luis', lesson1Controller.luis);
-router.get('/sandra', lesson1Controller.sandra);
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
